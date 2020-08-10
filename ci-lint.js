6b7fe96fe60a4663307d3e1db09ba03a5d0c9f04
Pablo Klaschka <contact@pabloklaschka.de>
2020-08-10 19:40:57 +0200

    Update ci-lint.js

--- ci-lint.js
+++ ci-lint.js
@@ -1,5 +1,10 @@
-console.log('::error file=.vale.ini,line=1,col=1::Test2')
 const fs = require('fs');
 const stdin = fs.readFileSync(0, 'utf-8');
 
-console.log('Vale output:', stdin)
+const result = JSON.parse(stdin);
+
+for (let file in result) {
+    for (let annotation of result[file]) {
+        console.log(`::error file=${file},line=${annotation.Line},col=${annotation.Span[0]}::${annotation.check}: ${annotation.Message}`)
+    }
+}
