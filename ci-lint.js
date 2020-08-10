0e37235de14b209815ca56841e17bb9e136ef3b6
Pablo Klaschka <contact@pabloklaschka.de>
2020-08-10 19:33:17 +0200

    Trying to route vale to NodeJS

--- ci-lint.js
+++ ci-lint.js
@@ -1,1 +1,5 @@
-console.log('::error file=.vale.ini,line=1,col=1::Test')
+console.log('::error file=.vale.ini,line=1,col=1::Test2')
+const fs = require('fs');
+const stdin = fs.readFileSync(0, 'utf-8');
+
+console.log('Vale output:', stdin)
