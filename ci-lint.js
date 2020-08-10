dea1527c2540e5bf476baaf9cb1998447c385bd5
Pablo Klaschka <contact@pabloklaschka.de>
2020-08-10 19:44:21 +0200

    Update ci-lint.js

--- ci-lint.js
+++ ci-lint.js
@@ -5,6 +5,6 @@ const result = JSON.parse(stdin);
 
 for (let file in result) {
     for (let annotation of result[file]) {
-        console.log(`::error file=${file},line=${annotation.Line},col=${annotation.Span[0]}::${annotation.check}: ${annotation.Message}`)
+        console.log(`::error file=${file},line=${annotation.Line},col=${annotation.Span[0]}::${annotation.Check}: ${annotation.Message}`)
     }
 }
