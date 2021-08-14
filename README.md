016b1bb82f45e017e637be970fc4dc363ffb8177
Pablo Klaschka <pklaschka@users.noreply.github.com>
2021-08-14 02:08:40 +0200

    feat!: Setup "Docusaurus 2"-based documentation
    Closes: #80

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
-# Ground Station Documentation
+# Telestion Docs
 
-![DITA-OT](https://github.com/TelestionGroup/telestion-docs/workflows/DITA-OT/badge.svg)
-
-The Telestion ground station documentation. 
-We use DITA.
-As editor, we recommend the [Oxygen XML Editor](https://www.oxygenxml.com/) (Win, macOS, Linux).
-For students of the JMU, a student version gets distributed on Studisoft.
-
-## Recommended Literature for getting started with DITA
-- _DITA – der topic-basierte XML-Standard: Ein schneller Einstieg (essentials)_ (**German!**) ([Amazon](https://www.amazon.de/DITA-topic-basierte-XML-Standard-schneller-essentials-ebook/dp/B01AWPHJTM), [Würzburg University ](https://bibliothek.uni-wuerzburg.de/permalink/bv/BV043209348)): A good introduction to writing DITA in general
-- _DITA Best Practices: A Roadmap for Writing, Editing, and Architecting in DITA (IBM Press) (English Edition)_ ([Amazon](https://www.amazon.de/DITA-Best-Practices-Roadmap-Architecting-ebook/dp/B005FEOU48/), [Würzburg University](https://katalog.bibliothek.uni-wuerzburg.de/TouchPoint/perma.do?q=+0%3D%22ZDB-30-ORH-047414049%22+IN+%5B2%5D&v=sunrise&l=de)): Best practices for writing DITA topics
-- [_Oxygen XML Editor Dita Editing Documentation_](https://www.oxygenxml.com/doc/versions/22.1/ug-editor/topics/author-dita.html)
-- [_DITA 1.3 Specs by OxygenXML_](https://www.oxygenxml.com/dita/1.3/specs/)
-
-## Attribution
-This repository uses the MIT-licensed [dita-ot-helper](https://github.com/fliegwerk/dita-ot-helper) and follows the MIT-licensed [dita-ot-helper-boilerplate](https://github.com/fliegwerk/dita-ot-helper-boilerplate) structure.
-
-Attribution to [fliegwerk](https://github.com/fliegwerk), as required by the license, is hereby provided.
+TODO
---
+++ docusaurus.config.js
@@ -0,0 +1,130 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+	title: 'Telestion Dev Hub',
+	tagline: 'A modular Ground Station',
+	url: 'https://wuespace.github.io/',
+	baseUrl: '/telestion-docs/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'wuespace', // Usually your GitHub org/user name.
+	projectName: 'telestion-docs', // Usually your repo name.
+	themeConfig: {
+		navbar: {
+			title: 'Telestion Docs',
+			logo: {
+				alt: 'Telestion Logo',
+				src: 'img/logo.svg'
+			},
+			items: [
+				// {
+				// 	type: 'doc',
+				// 	docId: 'intro',
+				// 	position: 'left',
+				// 	label: 'Documentation'
+				// },
+				{
+					href: 'https://telestion.wuespace.de',
+					label: 'Project Homepage',
+					position: 'right'
+				}
+			]
+		},
+		footer: {
+			style: 'light',
+			links: [
+				{
+					title: 'Telestion',
+					items: [
+						{
+							label: 'Homepage',
+							href: 'https://telestion.wuespace.de'
+						},
+						{
+							label: 'Project Documentation',
+							to: '/'
+						},
+						{
+							label: 'Development Blog',
+							href: 'https://telestion.wuespace.de/blog'
+						},
+						{
+							label: 'WüSpace e. V.',
+							href: 'https://www.wuespace.de'
+						}
+					]
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Twitter',
+							href: 'https://twitter.com/wuespace'
+						},
+						{
+							label: 'YouTube',
+							href: 'https://www.youtube.com/channel/UC1mwTgqCY5-G1RouptOXnSg'
+						}
+					]
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: "Writer's Guide",
+							href: '/writers-guide'
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/wuespace/telestion'
+						},
+						{
+							label: 'Legal Notice',
+							href: 'https://wuespace.de/legal-notice/'
+						},
+						{
+							label: 'Privacy Policy',
+							href: 'https://wuespace.de/privacy-policy/'
+						}
+					]
+				}
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} WüSpace e. V. Built with Docusaurus.`
+		},
+		prism: {
+			additionalLanguages: ['java', 'powershell'],
+			theme: lightCodeTheme,
+			darkTheme: darkCodeTheme
+		},
+		colorMode: {
+			disableSwitch: true,
+			respectPrefersColorScheme: false
+		}
+	},
+	clientModules: [],
+	plugins: [[require.resolve('@cmfcmf/docusaurus-search-local'), {
+		docsRouteBasePath: '/',
+		indexBlog: false
+	}]],
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+
+					routeBasePath: '/',
+					// Please change this to your repo.
+					editUrl: 'https://github.com/wuespace/telestion-docs'
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve('./src/css/custom.css')
+				}
+			}
+		]
+	]
+};
